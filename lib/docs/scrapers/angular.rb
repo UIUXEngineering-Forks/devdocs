4c22157406b74157f13ba6a8992f141c139c8a95
Jerry Orta <jerryorta.dev@gmail.com>
2016-01-21 06:45:32 -0600

    docs(angular 1.3.20):

--- lib/docs/scrapers/angular.rb
+++ lib/docs/scrapers/angular.rb
@@ -5,7 +5,7 @@ module Docs
     self.name = 'Angular.js'
     self.slug = 'angular'
     self.type = 'angular'
-    self.version = '1.4.8'
+    self.version = '1.3.20'
     self.base_url = "https://code.angularjs.org/#{version}/docs/partials/api/"
 
     html_filters.push 'angular/clean_html', 'angular/entries', 'title'
